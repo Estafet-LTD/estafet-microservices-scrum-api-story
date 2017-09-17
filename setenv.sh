4ff81767a7dc1281d1d447d290de6c90c9dbc14b
dennis williams <dennis.williams@estafet.com>
2017-09-17 17:26:18 +0100

    modified set environment scripts.

--- setenv.sh
+++ setenv.sh
@@ -1,5 +1,3 @@
-export JBOSS_A_MQ_BROKER_URL=tcp://localhost:61616
 export PROJECT_REPOSITORY_JDBC_URL=jdbc:postgresql://localhost:5432/project-repository
-export PROJECT_REPOSITORY_DB_USER=postgres
-export PROJECT_REPOSITORY_DB_PASSWORD=welcome1
+export STORY_REPOSITORY_JDBC_URL=jdbc:postgresql://localhost:5432/story-repository
 
