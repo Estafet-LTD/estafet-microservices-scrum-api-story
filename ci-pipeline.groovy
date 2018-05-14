a6bdff9b960d28bdbacf54cdf214ef3258bb573f
Dennis Williams <dennis.williams@estafet.com>
2018-05-14 23:09:54 +0100

    reset the env vars

--- ci-pipeline.groovy
+++ ci-pipeline.groovy
@@ -24,18 +24,16 @@ node('maven') {
 		sh "oc exec ${pod}  -n ${project} -- /bin/sh -i -c \"psql -d ${microservice} -U postgres -f /tmp/create-${microservice}-db.ddl\""
 	}
 
-	stage("build & deploy container") {
-		openshiftBuild namespace: project, buildConfig: microservice, showBuildLogs: "true",  waitTime: "300000"
-		openshiftVerifyDeployment namespace: project, depCfg: microservice, replicaCount:"1", verifyReplicaCount: "true", waitTime: "600000"
-	}
-	
 	stage("reset a-mq to purge topics") {
 		openshiftDeploy namespace: project, depCfg: "broker-amq", showBuildLogs: "true",  waitTime: "3000000"
 		openshiftVerifyDeployment namespace: project, depCfg: "broker-amq", replicaCount:"1", verifyReplicaCount: "true", waitTime: "300000"
-		sh "oc set env dc/${microservice} JBOSS_A_MQ_BROKER_URL=tcp://broker-amq-tcp.${project}.svc:61616 -n ${project}"
-		openshiftVerifyDeployment namespace: project, depCfg: microservice, replicaCount:"1", verifyReplicaCount: "true", waitTime: "600000"
 	}
-	
+
+	stage("build & deploy container") {
+		openshiftBuild namespace: project, buildConfig: microservice, showBuildLogs: "true",  waitTime: "300000", env : [ [ name : "JBOSS_A_MQ_BROKER_URL", value : "tcp://broker-amq-tcp.${project}.svc:61616" ] ]
+		openshiftVerifyDeployment namespace: project, depCfg: microservice, replicaCount:"1", verifyReplicaCount: "true", waitTime: "600000" 
+	}
+
 	stage("execute the container tests") {
 		withEnv(
 			[	"STORY_API_JDBC_URL=jdbc:postgresql://postgresql.${project}.svc:5432/${microservice}", 
