fd7901db5d13fbec55e1f3ebb9636b079e10c82c
Dennis Williams <dennis.williams@estafet.com>
2018-05-23 21:53:14 +0100

    fixes to incorectly named topic lookup

--- src/main/java/com/estafet/microservices/api/story/jms/UpdateSprintConsumer.java
+++ src/main/java/com/estafet/microservices/api/story/jms/UpdateSprintConsumer.java
@@ -14,6 +14,8 @@ import io.opentracing.Tracer;
 @Component
 public class UpdateSprintConsumer {
 
+	public final static String TOPIC = "update.sprint.topic";
+	
 	@Autowired
 	private Tracer tracer;
 	
@@ -23,10 +25,10 @@ public class UpdateSprintConsumer {
 	@Autowired
 	private MessageEventHandler messageEventHandler;
 
-	@JmsListener(destination = "update.sprint.topic", containerFactory= "myFactory")
+	@JmsListener(destination = TOPIC, containerFactory= "myFactory")
 	public void onMessage(String message, @Header("message.event.interaction.reference") String reference) {
 		try {
-			if (messageEventHandler.isValid("new.sprint.topic", reference)) {
+			if (messageEventHandler.isValid(TOPIC, reference)) {
 				storyService.updateSprint(Sprint.fromJSON(message));	
 			}
 		} finally {
