5b07d4f175445f2735975070e0ed5d9bfbf25ad9
Dennis Williams <dennyjoewilliams@yahoo.com>
2019-11-06 17:48:10 +0000

    initial commit standalone branch

--- src/integration-test/java/com/estafet/microservices/api/story/container/tests/NewStoryTopicConsumer.java
+++
@@ -1,9 +0,0 @@
-package com.estafet.microservices.api.story.container.tests;
-
-public class NewStoryTopicConsumer extends TopicConsumer {
-
-	public NewStoryTopicConsumer() {
-		super("new.story.topic");
-	}
-
-}
--- src/integration-test/java/com/estafet/microservices/api/story/container/tests/UpdatedStoryTopicConsumer.java
+++
@@ -1,9 +0,0 @@
-package com.estafet.microservices.api.story.container.tests;
-
-public class UpdatedStoryTopicConsumer extends TopicConsumer {
-
-	public UpdatedStoryTopicConsumer() {
-		super("update.story.topic");
-	}
-
-}
